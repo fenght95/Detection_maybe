0d053423c05824362a8ecb705376c99ccb560ce1
fenght95 <84504571@qq.com>
2021-01-22 10:53:57 +0800

    Update read_xml.py

--- read_xml.py
+++ read_xml.py
@@ -1,15 +1,13 @@
 import xml.etree.ElementTree as ET
 import os
-CLASSES = ('bus', 'bicycle', 'car', 'person', 'truck', 'tricycle')
 
 nums = {cat:0 for cat in CLASSES}
 
 
-xmls = os.listdir('./xml_demo/xml')
-#with open('./test_v.txt','r') as f:
-#    xmls = f.readlines()
+xmls = os.listdir('')
+
 for xml in xmls:
-    tree = ET.parse('./xml_demo/xml/' + xml.strip())
+    tree = ET.parse( + xml.strip())
     root = tree.getroot()
     for obj in root.findall('object'):
         name = obj.find('name').text
@@ -18,6 +16,4 @@ for xml in xmls:
         else:
             nums[name] = nums[name] + 1
 
-# ALL
-#{'bus': 2584, 'bicycle': 4060, 'car': 45449, 'person': 395, 'truck': 6194, 'tricycle': 19}
 print(nums)
